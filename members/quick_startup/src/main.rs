c4d521f8f873d7cc3b2e484a2d7b2c7c93e0556b
Michaël CHLON <mic.a.elle.chlon@gmail.com>
2026-03-29 16:54:50 +0200

    feat(quick_startup): add examples of mutability and shadowing in Rust
    Provide code in main.rs to illustrate variable mutability, assignment,
    and shadowing. Add .gitignore for target directory and Cargo.lock for
    dependency management.

--- members/quick_startup/src/main.rs
+++ members/quick_startup/src/main.rs
@@ -1,3 +1,19 @@
 fn main() {
-    println!("Hello, world!");
+    let my_age = 40;
+    println!("My age is {}", my_age);
+
+    let x1 = 40;
+    let mut x2 = x1;
+    x2 = x1 - 2;
+    println!("x1 is {} and x2 is {}", x1, x2); // x2 is 38 because x1 is immutable
+
+    let mut x1 = 40;
+    let x2;
+    x1 *= 3;
+    x2 = x1 - 2;
+    println!("x1 is: {}, x2 is: {}", x1, x2);
+
+    let a = "three"; // do not change this line
+    let a = 10; // do not change the name of variable 'a'
+    println!("a is: {}", a);
 }
