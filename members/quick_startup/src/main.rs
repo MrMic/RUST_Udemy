c3d813ed6b3b326de1572db877c37d6ed61383ab
Michaël CHLON <mic.a.elle.chlon@gmail.com>
2026-04-02 17:50:56 +0200

    feat(quick_startup): Add examples of compound data types

--- members/quick_startup/src/main.rs
+++ members/quick_startup/src/main.rs
@@ -1,19 +1,26 @@
+#![allow(unused_variables)]
+
 fn main() {
-    let my_age = 40;
-    println!("My age is {}", my_age);
+    // INFO: COMPOUND DATA TYPES
 
-    let x1 = 40;
-    let mut x2 = x1;
-    x2 = x1 - 2;
-    println!("x1 is {} and x2 is {}", x1, x2); // x2 is 38 because x1 is immutable
+    // Strings
+    let fixed_str = "Fixed length string"; // WARN: IMMUTABLE!
+    // WARN: String can grow/shrink
+    let mut flexible_str = String::from("Flexible length string");
+    flexible_str.push_str(" appended");
 
-    let mut x1 = 40;
-    let x2;
-    x1 *= 3;
-    x2 = x1 - 2;
-    println!("x1 is: {}, x2 is: {}", x1, x2);
+    // Arrays
+    let mut array_1: [i32; 5] = [1, 2, 3, 4, 5];
+    println!("{:?}", array_1[0]);
+    println!("{:?}", array_1);
 
-    let a = "three"; // do not change this line
-    let a = 10; // do not change the name of variable 'a'
-    println!("a is: {}", a);
+    // Vectors - WARN: Can grow and shrink!
+    let vec_1 = vec![1, 2, 3, 4, 5];
+
+    // Tuples
+    let my_info = ("salary", 40000, "Age", 25);
+    let (salary, salary_value, age, age_value) = my_info; // WARN: DESTRUCTURING 
+
+    // Empty Tuple - Not cosume any memory
+    let unit = ();
 }
