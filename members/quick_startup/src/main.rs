2327991409f5a786a4dd959a7c27b14388aa85c6
Michaël CHLON <mic.a.elle.chlon@gmail.com>
2026-04-06 10:36:36 +0200

    feat(quick_startup): replace function examples with match expression
    - Remove previous function and code block demonstrations.
    - Add an example of using the  expression with range patterns to calculate grades.

--- members/quick_startup/src/main.rs
+++ members/quick_startup/src/main.rs
@@ -1,55 +1,26 @@
 #![allow(unused_variables)]
 
 /*
-    - Functions
-    - Code Blocks
+    Conditional
+    - If else
+    - If else if ladder
+    - Match
+
 */
-// ______________________________________________________________________
-fn my_fn(s: &str) {
-    println!("{s}!");
-}
 
-// ______________________________________________________________________
-fn multiplication(x: i32, y: i32) -> i32 {
-    println!("Multiplying {x} * {y}"); // INFO: STATEMENT
-    x * y // INFO: EXPRESSION
-}
-
-// ______________________________________________________________________
-fn basic_math(num1: i32, num2: i32) -> (i32, i32, i32, f64) {
-    let sum = num1 + num2;
-    let difference = num1 - num2;
-    let product = num1 * num2;
-    let quotient = num1 as f64 / num2 as f64;
-    (sum, difference, product, quotient)
-}
-
-// INFO: EXPRESSION vs STATEMENTS _______________________________________
-// INFO: EXPRESSION is something that evaluates to a valid value.
-// INFO: STATEMENT is something that performs an action but does not return a value => a unit value
-
-// ______________________________________________________________________
 fn main() {
-    // my_fn("This my function");
-    let str = "This is my function";
-    my_fn(str);
+    // Match
+    let marks = 95;
+    // let mut grade = 'N';
 
-    let result = multiplication(5, 10);
-    println!("The result is: {result}");
-
-    let (sum, difference, product, quotient) = basic_math(10, 5);
-    println!("Sum: {sum}");
-    println!("Difference: {difference}");
-    println!("Product: {product}");
-    println!("Quotient: {quotient}");
-
-    // INFO: Code Blocks
-    let full_name = {
-        let first_name = "John";
-        let last_name = "Doe";
-        println!("First Name: {first_name}");
-        // INFO: Code block can also return values like functions
-        format!("{first_name} {last_name}")
+    let grade = match marks {
+        90..=100 => 'A',
+        80..=89 => 'B',
+        70..=79 => 'C',
+        60..=69 => 'D',
+        0..=59 => 'F',
+        _ => 'N', // Default case
     };
-    // println!("First Name: {first_name}"); // Inaccessible outside the block
+
+    println!("Grade: {}", grade);
 }
