b9dabea9d12efda9b21591443617d5749d36817e
Michaël CHLON <mic.a.elle.chlon@gmail.com>
2026-04-26 11:03:39 +0200

    feat(ownership): add ownership in functions examples - Ownership in Functions
    Demonstrates how functions take, give, and return ownership of heap
    data (Vec<i32>), and shows that stack data (i32) is copied by value.

--- members/ownership/src/main.rs
+++ members/ownership/src/main.rs
@@ -1,29 +1,57 @@
 /*
-* INFO: Ownership
-   - Each value in Rust has a variable that's called its owner.
-   - There can only be one owner at a time.
-   - When the owner goes out of scope, the value will be dropped.
+* INFO: ------------------------------------
+* Ownership in Functions
+*  - Functions that take ownership
+*  - Functions that give ownership
+*  - Functions that take and gives ownership
+*  -----------------------------------------
 */
 
 fn main() {
-    let s1 = String::from("hello");
-    let _s2 = s1;
-    // println!("s1 is {}", s1);⚠️ Borrow of moved value: `s1`
-    println!("s2 is {}", _s2);
+    // INFO:Functions that take ownership
+    let vec_1 = vec![1, 2, 3];
+    // takes_ownership(vec_1);  WARN: similar as vec = vec_1
+    // println!("The vector: {:?} has not been moved", vec_1); WARN: Borrow of moved value
+    takes_ownership(vec_1.clone());
 
-    let s3 = String::from("hello");
-    let s4 = s3.clone();
-    println!("s3 is {}, s4 is {}", s3, s4);
+    // INFO: Functions that give ownership back
+    println!("-----------------------------\n");
+    let vec_2 = gives_ownership();
+    println!("--> 👉️ vec_2 is now: {:?}", vec_2);
 
-    {
-        // INFO: Inner scope
-        let s5 = s3;
-        println!("INNER SCOPE => s5 is {}", s5);
-    }
-    // println!("s5 is {}", s5); WARN: use of moved value: `s5`
+    // INFO: Functions that take and returns ownership
+    println!("-----------------------------\n");
+    let vec_3 = takes_and_returns_ownership(vec_2);
+    // println!("--> 👉️ vec_2 is now: {:?}", vec_2); WARN: Borrow of moved value
+    println!("--> 👉️ vec_3 is now: {:?}", vec_3);
 
-    // INFO: Primitive types are Copy - No pointer to the Heap
-    let x = 5;
-    let y = x;
-    println!("x is {}, y is {}", x, y);
+    // INFO: Functions that take **Stack** Data
+    println!("-----------------------------\n");
+    let var = 42;
+    println!("--> 👉️ var was: {}", var);
+    stack_function(var);
+    println!("--> 👉️ var in main, is now: {}", var);
+}
+
+// ______________________________________________________________________
+fn takes_ownership(vec: Vec<i32>) {
+    println!("--> 👉️ The vector: {:?} has been moved", vec);
+}
+
+// ______________________________________________________________________
+fn gives_ownership() -> Vec<i32> {
+    println!("gives_ownership!");
+    vec![4, 5, 6]
+}
+
+// ______________________________________________________________________
+fn takes_and_returns_ownership(mut vec: Vec<i32>) -> Vec<i32> {
+    vec.push(7);
+    vec
+}
+
+// ______________________________________________________________________
+fn stack_function(mut var: i32) {
+    var = 56;
+    println!("--> 👉️ var in stack_function, is now: {}", var);
 }
